0fb5b937ae3b2ff5c951aefced59b91eda4a83cb
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-01-22 15:40:49 +0200

    first array

---
+++ JS.js
@@ -0,0 +1,59 @@
+let students=[{
+    startedAt: new Date("2021-01-20:13:00"),
+    finishedAt: new Date("2021-01-20:19:00"),
+    tasksGiven: 18,
+    tasksFinished: 7,
+    topic: 'JavaScript'
+},{
+    startedAt: new Date("2021-01-20:13:00"),
+    finishedAt: new Date("2021-01-20:19:00"),
+    tasksGiven: 21,
+    tasksFinished: 17,
+    topic: 'HTML'
+},{
+    startedAt: new Date("2021-01-20:13:00"),
+    finishedAt: new Date("2021-01-20:19:00"),
+    tasksGiven: 15,
+    tasksFinished: 6,
+    topic: 'Arrays'
+},{
+    startedAt: new Date("2021-01-20:13:00"),
+    finishedAt: new Date("2021-01-20:19:00"),
+    tasksGiven: 12,
+    tasksFinished: 7,
+    topic: 'Loops'
+},{
+    startedAt: new Date("2021-01-20:13:00"),
+    finishedAt: new Date("2021-01-20:19:00"),
+    tasksGiven: 13,
+    tasksFinished: 2,
+    topic: 'Github'
+},{
+    startedAt: new Date("2021-01-20:13:00"),
+    finishedAt: new Date("2021-01-20:19:00"),
+    tasksGiven: 19,
+    tasksFinished: 15,
+    topic: ''
+},{
+    startedAt: new Date("2021-01-20:13:00"),
+    finishedAt: new Date("2021-01-20:19:00"),
+    tasksGiven: 15,
+    tasksFinished: 14,
+    topic: JavaScript
+},{
+    startedAt: new Date("2021-01-20:13:00"),
+    finishedAt: new Date("2021-01-20:19:00"),
+    tasksGiven: 12,
+    tasksFinished: 10,
+    topic: JavaScript
+},{    startedAt: new Date("2021-01-20:13:00"),
+finishedAt: new Date("2021-01-20:19:00"),
+tasksGiven: 14,
+tasksFinished: 7,
+topic: JavaScript},{
+    startedAt: new Date("2021-01-20:13:00"),
+    finishedAt: new Date("2021-01-20:19:00"),
+    tasksGiven: 25,
+    tasksFinished: 5,
+    topic: JavaScript
+}]
