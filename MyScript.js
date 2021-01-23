eff9ff081951ee3044b201a6886210ab4be61866
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-01-23 11:08:14 +0200

    Update MyScript.js

--- MyScript.js
+++ MyScript.js
@@ -1,6 +1,6 @@
 let students=[
     {
-    startedAt: new Date("2021-01-20:13:30"),
+    startedAt: new Date("2021-01-20:13:00"),
     finishedAt: new Date("2021-01-20:19:00"),
     tasksGiven: 18,
     tasksFinished: 7,
